b95be95b82fbaede9510192f2ce18c259d0c2c19
William <williamwallace424@yahoo.co.nz>
2021-11-11 21:43:07 +1300

    adding full stop

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,7 @@ const Projects = props => {
         {ProjectCard(props, "/images/cokiexplorer1.png", "COKI Explorer", "My final year university project, developing a research publication search engine using React and Redux.", "React • Redux • Firebase", "https://github.com/williamlwallace/coki-explorer")}
         {ProjectCard(props, "/images/travelea.png", "TravelEA", "Full stack travel/social media web application developed in a team of eight using agile methodologies during my third year at University.", "HTML5 • CSS • JavaScript • Java", "https://github.com/williamlwallace/travel-ea")}
         {ProjectCard(props, "/images/crypto-bot.png", "CryptoBot", "Discord bot and separate API that can be used to quickly retrieve crypto prices, data and more!", "Python • Node.js", "https://github.com/williamlwallace/crypto-bot")}
-        {ProjectCard(props, "/images/rogur.png", "rogur", "rogur is a clone of the Uber app built using a MERN stack", "React Native • Redux • Express • MongoDB", "https://github.com/williamlwallace/rogur")}      
+        {ProjectCard(props, "/images/rogur.png", "rogur", "rogur is a clone of the Uber app built using a MERN stack.", "React Native • Redux • Express • MongoDB", "https://github.com/williamlwallace/rogur")}      
         {ProjectCard(props, "/images/covidaily1.png", "COVIDaily", "Simple android app built using Kotlin that retireves and displays relevant statistics/news about COVID-19 in your country.", "Kotlin", "https://github.com/williamlwallace/covid-daily")}
         {ProjectCard(props, "/images/pocketchef1.PNG", "PocketCHEF", "Simple android app built using Kotlin that allows you to create, view and share cooking/baking recipes.", "Kotlin • Firebase", "https://github.com/williamlwallace/pocket-chef")}
       </div>
